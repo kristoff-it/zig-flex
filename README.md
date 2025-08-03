b72501c1bb2048d9e020f40c7e9182d6451a6a25
Loris Cro <kappaloris@gmail.com>
2025-08-03 19:28:19 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # zig-flex
 Like C's flexible array members but better :^)
+
+## TODOs
+
+- [ ] Packed struct support (see Zig [#20458](https://github.com/ziglang/zig/issues/20458))
+- [ ] Support more than one void array and improve API accordingly
+
+The library is otherwise in working condition, albeit not very well battle tested.
+
+## Abstract
+Imagine that you have a `Person` struct that has an age and a name.
+This is how you would define it normally:
+
+```zig
+const Person = struct {
+  age: usize,
+  name: []const u8,
+};
+```
+
+Note how this memory layout implies that a person and a name are expected to
+be distinct allocations, which you can connect through the `Person.name` slice
+pointer.
+
+```
++--------+
+| PERSON |
++--------+
+| AGE    |    +-----------+
+| NAME --|--->| LORIS CRO |
++--------+    +-----------+
+```
+
+In some circumstances it's instead desireable for closely-related data to always
+be stored as a single allocation. This means allocating enough memory for a
+instance of `Person` **and** also enough memory to store the full name.
+
+Additionally, in such case you would not want to use a slice type because you
+don't need to store any pointer in `Person` if the bytes for `name` are
+immediately after the struct.
+
+```
++------------+
+|   PERSON   |
++------------+
+| AGE        |
+| NAME_LEN   |  
++------------+
+| LORIS CRO  |
++------------+  
+```
+
+Without any abstraction, this is how you would write this in Zig:
+
+```zig
+const Person = struct {
+  age: usize,
+  name_len: usize,
+
+  pub fn create(gpa: Allocator, age: usize, name_len: usize) *Person {
+    const size: usize = @sizeOf(Person) + name_len;
+    const bytes = gpa.allocWithOptions(u8, size, .of(Person), null);
+    const p: *Person = @ptrCast(bytes);
+    p.* = .{
+      .age = age,
+      .name_len = name_len,
+    };
+
+    return p;
+  }
+
+  pub fn destroy(p: *Person, gpa: Allocator) void {
+    const bytes: [*]u8 = @ptrCast(p);
+    gpa.free(bytes[0..(@sizeOf(Person) + p.name_len)]);
+  }
+ 
+  pub fn name(p: *Person) []u8 {
+      const bytes: [*]u8 = @ptrCast(p);
+      return bytes[@sizeOf(Person)..][0..p.name]; 
+  }
+}
+```
+
+The C programming language uses [flexible array member](https://en.wikipedia.org/wiki/Flexible_array_member) to give programmers a convenient way of accessing the
+memory right after the end of a struct.
+
+Real world scenarios where this technique is used:
+- [antirez/rax](https://github.com/antirez/rax/)
+
+Flex is a more type-safe implementation of flexible array members from C.
+
+## Usage
+
+```zig
+const flex = @import("flex");
+
+const Person = struct {
+  age: u32,
+  name: flex.Array(u8, .{
+    .parent = Person,
+    .field_name = .name,
+  }),
+  surname: flex.Array(u8, .{
+    .parent = Person,
+    .field_name = .surname,
+  }),
+}
+  
+
+pub fn main () !void {
+  const gpa = std.heap.smp_allocator;
+  const p = try flex.create(Person, gpa, .{
+    .name = 5,
+    .surname = 3,
+  });
+
+  p.age = 30;
+  p.name.init("loris"); // will panic if length does not match
+  p.surname.init("cro"); // will panic if length does not match
+  
+  std.debug.print("name = {s} surname = {s}\n",. {
+     p.name.slice(),
+     p.surname.slice(),
+  });
+
+  const p1 = try flex.resize(Person, gpa, p, null, .{
+    .name = 8,
+    .surname = 0,
+  }) orelse p;
+
+  p1.name.init("kristoff");
+}
+```
--- src/root.zig
+++ src/root.zig
@@ -7,40 +7,45 @@ pub const Options = struct {
     parent: type,
     /// The enum literal matching the field name of this flexible array.
     name: @Type(.enum_literal),
-    /// The integer type to be used to store length information for the
-    /// flexible array. To make the flexible array not store length information
-    /// in the struct, use `void`.
+    /// The type to be used to store length information for the flexible array.
     ///
-    /// Note that only the last field of a struct can have `lenght_type` set to
-    /// `void`.
+    /// Normally you will want to provide an integer type (e.g. u32, u27), but
+    /// `void` and `bool` are also allowed:
     ///
-    /// When a flexible array has a `void` lenght type, the `slice` function
-    /// will return a [*]T. When present, length information will be stored in
-    /// a packed struct so FlexibleArray can be used with extern and packed
-    /// structs.
+    /// - `bool` will allow you to use `.get()` instead of `.slice()` on this
+    ///    field
+    /// - `void` will make the flexible array *NOT* store length information
+    ///    which you will have to track externally (e.g. by deriving it from
+    ///    other state in the struct) and occasionally provide to flex APIs.
+    ///
+    /// When a flexible array has a `void` length type, `.slice()` will return
+    /// a [*]T. When present, length information will be stored in a packed
+    /// struct so FlexibleArray can also be used with extern and packed structs.
     ///
     /// When length type is not `void`, you can access length information via
     /// `field.len`.
     length_type: type = usize,
 };
 
-pub fn FlexibleArray(T: type, opts: Options) type {
+pub fn Array(T: type, opts: Options) type {
     const P = opts.parent;
 
     return packed struct {
-        len: opts.length_type,
+        len: LengthType,
 
         const IsFlexibleArray = {};
         const Child = T;
+        const LengthType = opts.length_type;
         comptime {
             const pinfo = @typeInfo(P).@"struct"; // opts.parent must be a struct
-            const leninfo = @typeInfo(opts.length_type);
-            if (opts.length_type == void) {
-                if (pinfo.fields[pinfo.fields.len - 1].type != @This()) {
+            switch (LengthType) {
+                bool => {},
+                void => if (pinfo.fields[pinfo.fields.len - 1].type != @This()) {
                     @compileError("Only the last field of a struct can nave a `null` length type");
-                }
-            } else if (leninfo != .int) {
-                @compileError("Len must be an integer type or void");
+                },
+                else => if (@typeInfo(LengthType) != .int) {
+                    @compileError("Len must be an integer type or void");
+                },
             }
         }
 
@@ -49,8 +54,32 @@ pub fn FlexibleArray(T: type, opts: Options) type {
             @memcpy(s, values); // 'values' has different len than allocated memory
         }
 
-        const ReturnType = if (opts.length_type == void) [*]T else []T;
+        /// Returns a pointer to the flexible value, only available when
+        /// length_type is `bool`.
+        pub fn getPtr(f: *@This()) ?*T {
+            if (LengthType != bool) @compileError(
+                "get is only available for flexible arrays of length type 'bool'",
+            );
+
+            return if (f.len) &f.sliceImpl()[0] else null;
+        }
+
+        /// Returns a slice to the flexible array. If the flexible array has
+        /// length_type set to `void`, the return value will be [*]T, otherwise
+        /// it will be []T.
+        ///
+        /// Not available when length_type is `bool`, use `get` instead.
         pub fn slice(f: *@This()) ReturnType {
+            return switch (LengthType) {
+                bool => @compileError(
+                    "use 'get' on flexible arrays of length type 'bool'",
+                ),
+                else => f.sliceImpl(),
+            };
+        }
+
+        const ReturnType = if (LengthType == void) [*]T else []T;
+        fn sliceImpl(f: *@This()) ReturnType {
             const pinfo = @typeInfo(P).@"struct"; // opts.parent must be a struct
             const name = @tagName(opts.name);
 
@@ -64,22 +93,91 @@ pub fn FlexibleArray(T: type, opts: Options) type {
 
                 if (!is_flexible) continue;
 
-                offset = std.mem.alignForward(usize, offset, @alignOf(T));
+                offset = std.mem.alignForward(usize, offset, @alignOf(field.type.Child));
 
-                const value = @field(parent, field.name);
                 if (std.mem.eql(u8, field.name, name)) {
                     const items: [*]T = @alignCast(@ptrCast(bytes[offset..]));
-                    if (opts.length_type == void)
-                        return items
-                    else
-                        return items[0..value.len];
+                    if (ReturnType == [*]T and LengthType == void) {
+                        return items;
+                    } else switch (field.type.LengthType) {
+                        void => unreachable,
+                        else => { // int
+                            const value_len = intLen(@field(parent, field.name).len);
+                            return items[0..value_len];
+                        },
+                    }
                 }
 
-                offset += value.len * @sizeOf(field.type.Child);
+                if (field.type.LengthType != void) {
+                    const value_len: usize = intLen(@field(parent, field.name).len);
+                    offset += value_len * @sizeOf(field.type.Child);
+                }
             }
             unreachable;
         }
     };
+}
+
+pub fn Lengths(P: type) type {
+    const fields = @typeInfo(P).@"struct".fields; // P must be a struct
+    var buf: [fields.len]std.builtin.Type.StructField = undefined;
+    var idx = 0;
+    inline for (fields) |field| {
+        const is_flexible = @typeInfo(field.type) == .@"struct" and
+            @hasDecl(field.type, "IsFlexibleArray");
+
+        if (!is_flexible) continue;
+        const OriginalT = field.type.LengthType;
+        const T = if (OriginalT == void) usize else OriginalT;
+        buf[idx] = .{
+            .name = field.name,
+            .type = T,
+            .default_value_ptr = null,
+            .is_comptime = false,
+            .alignment = @alignOf(T),
+        };
+        idx += 1;
+    }
+
+    return @Type(.{
+        .@"struct" = .{
+            .fields = buf[0..idx],
+            .layout = .auto,
+            .is_tuple = false,
+            .decls = &.{},
+        },
+    });
+}
+
+pub fn SomeLengths(P: type) type {
+    const fields = @typeInfo(P).@"struct".fields; // P must be a struct
+    var buf: [fields.len]std.builtin.Type.StructField = undefined;
+    var idx = 0;
+    inline for (fields) |field| {
+        const is_flexible = @typeInfo(field.type) == .@"struct" and
+            @hasDecl(field.type, "IsFlexibleArray");
+
+        if (!is_flexible) continue;
+        const OriginalT = field.type.LengthType;
+        const T = if (OriginalT == void) usize else OriginalT;
+        buf[idx] = .{
+            .name = field.name,
+            .type = ?T,
+            .default_value_ptr = @as(*const ?T, &null),
+            .is_comptime = false,
+            .alignment = @alignOf(?T),
+        };
+        idx += 1;
+    }
+
+    return @Type(.{
+        .@"struct" = .{
+            .fields = buf[0..idx],
+            .layout = .auto,
+            .is_tuple = false,
+            .decls = &.{},
+        },
+    });
 }
 
 /// Creates an instance of a struct with FlexibleArray fields.
@@ -89,7 +187,7 @@ pub fn create(
     P: type,
     gpa: Allocator,
     /// A struct that contains a field for each FlexibleArray field in `P`.
-    lengths: anytype,
+    lengths: Lengths(P),
 ) error{OutOfMemory}!*P {
     const pinfo = @typeInfo(P).@"struct"; // P must be a struct
     var size: usize = @sizeOf(P);
@@ -99,12 +197,10 @@ pub fn create(
             @hasDecl(field.type, "IsFlexibleArray");
 
         if (!is_flexible) continue;
-        if (!@hasField(@TypeOf(lengths), field.name)) {
-            @compileError("lengths is missing '" ++ field.name ++ "'");
-        }
 
-        size = std.mem.alignForward(usize, size, @alignOf(field.type));
-        size += @field(lengths, field.name);
+        const field_len: usize = intLen(@field(lengths, field.name));
+        size = std.mem.alignForward(usize, size, @alignOf(field.type.Child));
+        size += field_len * @sizeOf(field.type.Child);
     }
 
     const data = try gpa.allocWithOptions(u8, size, .of(P), null);
@@ -115,7 +211,7 @@ pub fn create(
             @hasDecl(field.type, "IsFlexibleArray");
 
         if (!is_flexible) continue;
-        if (field.type.Child != void) {
+        if (field.type.LengthType != void) {
             @field(p, field.name).len = @field(lengths, field.name);
         }
     }
@@ -147,17 +243,163 @@ pub fn destroy(
 
         if (!is_flexible) continue;
 
-        size = std.mem.alignForward(usize, size, @alignOf(field.type));
+        size = std.mem.alignForward(usize, size, @alignOf(field.type.Child));
 
-        if (field.type.Child == void) {
-            size += void_len.?; // missing void_len!
-        } else {
-            size += @field(p, field.name).len;
+        switch (field.type.LengthType) {
+            void => {
+                size += void_len.? * @sizeOf(field.type.Child); // missing void_len!
+            },
+            else => {
+                const field_len: usize = intLen(@field(p, field.name).len);
+                size += field_len * @sizeOf(field.type.Child);
+            },
         }
+
+        if (std.meta.fieldInfo(field.type, .len).type != void) {} else {}
     }
 
     const bytes: [*]align(@alignOf(P)) u8 = @ptrCast(p);
     gpa.free(bytes[0..size]);
+}
+
+/// Resizes a flexible struct. If resizing requires moving the whole struct, a
+/// pointer to the new location will be returned.
+/// Resizing will also move data according to the new lengths provided.
+///
+/// For a given field:
+///
+/// - new_length > old_length:
+///      all old data will be copied, new bytes will be set to undefined
+///
+/// - old_length > new_length:
+///      only old data that fits the new length will be copied, discarding old
+///      data starting from the end. e.g.: [1, 2, 3, 4] -> [1, 2, 3]
+pub fn resize(
+    P: type,
+    gpa: Allocator,
+    p: *P,
+    void_len: ?usize,
+    // Struct that contains all lenghts that must change. You can omit lenghts
+    // that you intend to leave untouched.
+    new_lengths: SomeLengths(P),
+) !?*P {
+    const original: P = p.*;
+    errdefer {
+        const linfo = @typeInfo(@TypeOf(new_lengths)).@"struct";
+        inline for (linfo.fields) |field| {
+            if (@TypeOf(@field(p, field.name).len) != void) {
+                @field(p, field.name).len = @field(original, field.name).len;
+            }
+        }
+    }
+
+    var deltas: [flexibleFieldsCount(P)]struct {
+        old_offset: usize,
+        new_offset: usize,
+        size: usize,
+    } = undefined;
+    var di: usize = deltas.len - 1;
+
+    const pinfo = @typeInfo(P).@"struct"; // P must be a struct
+    var old_size: usize = @sizeOf(P);
+    var new_size: usize = @sizeOf(P);
+    inline for (pinfo.fields) |field| {
+        const is_flexible = @typeInfo(field.type) == .@"struct" and
+            @hasDecl(field.type, "IsFlexibleArray");
+
+        if (!is_flexible) continue;
+
+        old_size = std.mem.alignForward(usize, old_size, @alignOf(field.type.Child));
+        new_size = std.mem.alignForward(usize, new_size, @alignOf(field.type.Child));
+
+        deltas[di].old_offset = old_size;
+        deltas[di].new_offset = new_size;
+        defer di -|= 1;
+
+        switch (field.type.LengthType) {
+            else => {
+                const field_len: usize = intLen(@field(p, field.name).len);
+                const field_size = field_len * @sizeOf(field.type.Child);
+                old_size += field_size;
+
+                if (@field(new_lengths, field.name)) |new_field_len| {
+                    const usize_new_field_len: usize = intLen(new_field_len);
+                    const new_field_size = usize_new_field_len * @sizeOf(field.type.Child);
+                    new_size += new_field_size;
+                    @field(p, field.name).len = new_field_len;
+                    deltas[di].size = @min(field_size, new_field_size);
+                } else {
+                    new_size += field_len * @sizeOf(field.type.Child);
+                    deltas[di].size = field_size;
+                }
+            },
+            void => {
+                const void_size = void_len.? * @sizeOf(field.type.Child); // missing void_len!
+
+                old_size += void_size;
+                if (@field(new_lengths, field.name)) |new_field_len| {
+                    const usize_new_void_len: usize = new_field_len;
+                    const new_void_size = usize_new_void_len * @sizeOf(field.type.Child);
+                    new_size += new_void_size;
+                    deltas[di].size = @min(void_size, new_void_size);
+                } else {
+                    new_size += void_size;
+                    deltas[di].size = void_size;
+                }
+            },
+        }
+    }
+
+    const bytes: [*]align(@alignOf(P)) u8 = @ptrCast(p);
+    const new = gpa.remap(bytes[0..old_size], new_size) orelse {
+        // remap failed, do everything manually
+
+        const data = try gpa.allocWithOptions(u8, new_size, .of(P), null);
+        const new: *P = @ptrCast(data);
+        new.* = p.*;
+
+        for (deltas[di..]) |d| @memcpy(
+            data[d.new_offset..][0..d.size],
+            bytes[d.old_offset..][0..d.size],
+        );
+
+        gpa.free(bytes[0..old_size]);
+        return new;
+    };
+
+    for (deltas[di..]) |d| @memmove(
+        new[d.new_offset..][0..d.size],
+        new[d.old_offset..][0..d.size],
+    );
+
+    if (new.ptr == bytes) return null;
+    return @ptrCast(new.ptr);
+}
+
+fn flexibleFieldsCount(P: type) comptime_int {
+    var count = 0;
+    const pinfo = @typeInfo(P).@"struct"; // P must be a struct
+    inline for (pinfo.fields) |field| {
+        const is_flexible = @typeInfo(field.type) == .@"struct" and
+            @hasDecl(field.type, "IsFlexibleArray");
+
+        if (!is_flexible) continue;
+        count += 1;
+    }
+
+    return count;
+}
+
+fn intLen(l: anytype) switch (@TypeOf(l)) {
+    bool => u1,
+    void => unreachable,
+    else => |t| t,
+} {
+    return switch (@TypeOf(l)) {
+        bool => @intFromBool(l),
+        void => unreachable,
+        else => l,
+    };
 }
 
 test {
@@ -165,12 +407,12 @@ test {
         is_key: bool,
         is_null: bool,
         layout: enum(u1) { merge, split },
-        bytes: FlexibleArray(u8, .{
+        bytes: Array(u8, .{
             .parent = @This(),
             .name = .bytes,
             .length_type = u29,
         }),
-        ptrs: FlexibleArray(*@This(), .{
+        ptrs: Array(*@This(), .{
             .parent = @This(),
             .name = .ptrs,
             .length_type = void,
@@ -183,11 +425,11 @@ test {
 test "alignment" {
     const Foo = struct {
         bar: u32,
-        baz: FlexibleArray(u8, .{
+        baz: Array(u8, .{
             .parent = @This(),
             .name = .baz,
         }),
-        bax: FlexibleArray(u64, .{
+        bax: Array(u64, .{
             .parent = @This(),
             .name = .bax,
         }),
@@ -212,15 +454,13 @@ test {
     const Packet = struct {
         id: u32,
         hash: u32,
-        name: FlexibleArray(u8, .{
+        name: Array(u8, .{
             .parent = @This(),
             .name = .name,
-            .length_type = usize,
         }),
-        label: FlexibleArray(u8, .{
+        label: Array(u8, .{
             .parent = @This(),
             .name = .label,
-            .length_type = usize,
         }),
     };
 
